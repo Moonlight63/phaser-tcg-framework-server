fb37c0fceaca59d8b2a4c6fbec4c12d71715963b
Dalen Catt <dalencattmlsp@gmail.com>
2023-08-19 04:30:07 -0700

    Refactored `ServerState.ts` to export individual variables and a `create` function for initializing the storage connectors.

--- ServerState.ts
+++ ServerState.ts
@@ -6,36 +6,34 @@ import { InMemorySessionStorage } from './connectors/session/InMemorySessionStor
 import { LobbyStorageConnector } from './connectors/lobby/LobbyStorageConnector';
 import { InMemoryLobbyStorage } from './connectors/lobby/InMemoryLobbyStorage';
 
-export class ServerState {
-  userStorage: UserStorageConnector;
-  sessionStorage: SessionStorageConnector;
-  lobbyStorage: LobbyStorageConnector;
+export let userStorage: UserStorageConnector;
+export let sessionStorage: SessionStorageConnector;
+export let lobbyStorage: LobbyStorageConnector;
 
-  constructor(config: Parsed) {
-    // Initialize the connectors based on the config
-    switch (config.userStorage.type) {
-      case 'LocalConnector':
-        this.userStorage = new LocalUserStorage(config.userStorage.filePath);
-        break;
-      // Here, you can expand to other types like PostgreSQL, etc.
-      default:
-        throw new Error('User storage type not yet implemented');
-    }
+export function create(config: Parsed) {
+  // Initialize the connectors based on the config
+  switch (config.userStorage.type) {
+    case 'LocalConnector':
+      userStorage = new LocalUserStorage(config.userStorage.filePath);
+      break;
+    // Here, you can expand to other types like PostgreSQL, etc.
+    default:
+      throw new Error('User storage type not yet implemented');
+  }
 
-    switch (config.sessionStorage.type) {
-      case 'InMemory':
-        this.sessionStorage = new InMemorySessionStorage();
-        break;
-      default:
-        throw new Error('Session storage type not yet implemented');
-    }
+  switch (config.sessionStorage.type) {
+    case 'InMemory':
+      sessionStorage = new InMemorySessionStorage();
+      break;
+    default:
+      throw new Error('Session storage type not yet implemented');
+  }
 
-    switch (config.lobbyStorage.type) {
-      case 'InMemory':
-        this.lobbyStorage = new InMemoryLobbyStorage();
-        break;
-      default:
-        throw new Error('Lobby storage type not yet implemented');
-    }
+  switch (config.lobbyStorage.type) {
+    case 'InMemory':
+      lobbyStorage = new InMemoryLobbyStorage();
+      break;
+    default:
+      throw new Error('Lobby storage type not yet implemented');
   }
 }
