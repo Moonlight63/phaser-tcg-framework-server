7353e94f83f1036e2b2697ea6b1eba87d5559022
Dalen Catt <dalencattmlsp@gmail.com>
2023-08-19 03:20:39 -0700

    Added types for decks and cards in the User type.

--- types/User.ts
+++ types/User.ts
@@ -2,4 +2,19 @@ export type User = {
     id: string;
     username: string;
     password: string;
+    decks: Array<Deck>;
 };
+
+export type Deck = {
+    id: string;
+    title: string;
+    cards: Array<Card>;
+};
+
+export type Card = {
+    id: string;
+    name: string;
+    description: string;
+    pointValue: number;
+    imageUrl: string;
+}
