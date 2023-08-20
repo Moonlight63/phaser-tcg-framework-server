be454360474ea0e42afd6b3ea36c09abbdf88b79
Dalen Catt <dalencattmlsp@gmail.com>
2023-08-20 08:56:28 -0700

    Added default values to the `defaultOptions` object in the constructor of `FileSessionStorage` class.

--- connectors/session/FileSessionStore.ts
+++ connectors/session/FileSessionStore.ts
@@ -8,7 +8,13 @@ export class FileSessionStorage extends SessionStorageConnector {
 
   constructor(config: SessionStorageOptions['FileStore']) {
     super();
-    const defaultOptions = {}; // Default options can be set here
+    const defaultOptions = {
+      ttl: 3600, // Session time to live in seconds. Defaults to `3600`
+      retries: 5, // The number of retries to get session data from a session file. Defaults to `5`
+      factor: 1, // The exponential factor to use for retry. Defaults to `1`
+      minTimeout: 50, // The number of milliseconds before starting the first retry. Defaults to `50`
+      maxTimeout: 100, // The maximum number of milliseconds between two retries. Defaults to `100`
+    };
     const options = { ...defaultOptions, ...config };
     const FileStore = FileStoreFactory(session);
     this.store = new FileStore(options);
