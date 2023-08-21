e8739f29830fa59dd14228dfcec3fe7916bde023
Dalen Catt <dalencattmlsp@gmail.com>
2023-08-21 06:13:57 -0700

    Refactored `ServerState.ts` to not use a static function and instead use plain exported variables and functions.

--- ServerState.ts
+++ ServerState.ts
@@ -7,64 +7,44 @@ import { LobbyStorageConnector } from './connectors/lobby/LobbyStorageConnector'
 import { InMemoryLobbyStorage } from './connectors/lobby/InMemoryLobbyStorage';
 
 
-class StateFactory {
-  private static _userStorage: UserStorageConnector;
-  private static _sessionStorage: SessionStorageConnector;
-  private static _lobbyStorage: LobbyStorageConnector;
+let UserStorage: UserStorageConnector;
+let SessionStorage: SessionStorageConnector;
+let LobbyStorage: LobbyStorageConnector;
 
-  static get UserStorage() {
-    return this._userStorage;
+async function create(config: Parsed) {
+  console.log("Creating state...");
+  // Initialize the connectors based on the config
+  switch (config.userStorage.type) {
+    case 'FileStore':
+      console.log("Creating user storage...");
+      UserStorage = new LocalUserStorage(config.userStorage);
+      console.log("User storage created.");
+      break;
+    // Here, you can expand to other types like PostgreSQL, etc.
+    default:
+      throw new Error('User storage type not yet implemented');
   }
 
-  static get SessionStorage() {
-    return this._sessionStorage;
+  switch (config.sessionStorage.type) {
+    case 'FileStore':
+      console.log("Creating session storage...");
+      SessionStorage = new FileSessionStorage(config.sessionStorage);
+      console.log("Session storage created.");
+      break;
+    default:
+      throw new Error('Session storage type not yet implemented');
   }
 
-  static get LobbyStorage() {
-    return this._lobbyStorage;
+  switch (config.lobbyStorage.type) {
+    case 'InMemory':
+      console.log("Creating lobby storage...");
+      LobbyStorage = new InMemoryLobbyStorage();
+      console.log("Lobby storage created.");
+      break;
+    default:
+      throw new Error('Lobby storage type not yet implemented');
   }
-
-  static async create(config: Parsed) {
-    console.log("Creating state factory...");
-    // Initialize the connectors based on the config
-    switch (config.userStorage.type) {
-      case 'FileStore':
-        console.log("Creating user storage...");
-        StateFactory._userStorage = new LocalUserStorage(config.userStorage);
-        console.log("User storage created.");
-        break;
-      // Here, you can expand to other types like PostgreSQL, etc.
-      default:
-        throw new Error('User storage type not yet implemented');
-    }
-
-    switch (config.sessionStorage.type) {
-      case 'FileStore':
-        console.log("Creating session storage...");
-        StateFactory._sessionStorage = new FileSessionStorage(config.sessionStorage);
-        console.log("Session storage created.");
-        break;
-      default:
-        throw new Error('Session storage type not yet implemented');
-    }
-
-    switch (config.lobbyStorage.type) {
-      case 'InMemory':
-        console.log("Creating lobby storage...");
-        StateFactory._lobbyStorage = new InMemoryLobbyStorage();
-        console.log("Lobby storage created.");
-        break;
-      default:
-        throw new Error('Lobby storage type not yet implemented');
-    }
-    console.log("State factory created.");
-  }
+  console.log("State created.");
 }
 
-// Named exports for each static getter.
-export const UserStorage = StateFactory.UserStorage;
-export const SessionStorage = StateFactory.SessionStorage;
-export const LobbyStorage = StateFactory.LobbyStorage;
-export function CreateState(config: Parsed) {
-  StateFactory.create(config);
-}
+export { UserStorage, SessionStorage, LobbyStorage, create as CreateState };
