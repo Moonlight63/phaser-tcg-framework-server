1b94438ebac4264f6de57328a8c89d452b9abb6d
Dalen Catt <dalencattmlsp@gmail.com>
2023-09-03 05:32:11 -0700

    Refactor lobbyRouter to remove authentication middleware and handle authentication checks within the route handlers.

--- lobby/lobbyRouter.ts
+++ lobby/lobbyRouter.ts
@@ -1,14 +1,25 @@
 import express from 'express';
-import { ensureAuthenticated } from '../authentication/LocalStrategy';
 
 const router = express.Router();
 
-router.post('/create', ensureAuthenticated, (req, res) => {
+router.post('/create', (req, res) => {
   // Logic for creating a lobby goes here
+  if (req.user) {
+    // Create a lobby
+    // Save the lobby to the database
+    // Return the lobby to the user
+  } else {
+    res.send("Not Logged In");
+  }
 });
 
-router.post('/join', ensureAuthenticated, (req, res) => {
+router.post('/join', (req, res) => {
   // Logic for joining a lobby goes here
+  if (req.user) {
+    // Join a lobby
+  } else {
+    res.send("Not Logged In");
+  }
 });
 
 export default router;
