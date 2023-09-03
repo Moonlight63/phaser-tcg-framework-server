5e93b4a0430e8b0ce46a7ff37d5a4e9a216e93bf
Dalen Catt <dalencattmlsp@gmail.com>
2023-09-03 05:32:52 -0700

    aider: Refactored lobbyRouter to handle lobby creation and joining with authentication.
    # Aider chat conversation:
    
    USER: You were still missing the point with the authentication. I have modified the code in `lobby/lobbyRouter.ts` to reflect what I meant. Please fill out the rest where I left comments.
    
    ASSISTANT: Understood. Let's proceed with the changes to the `lobby/lobbyRouter.ts` file. We will fill out the logic for creating and joining lobbies.
    
    ```typescript
    lobby/lobbyRouter.ts
    <<<<<<< HEAD
    router.post('/create', (req, res) => {
      // Logic for creating a lobby goes here
      if (req.user) {
        // Create a lobby
        // Save the lobby to the database
        // Return the lobby to the user
      } else {
        res.send("Not Logged In");
      }
    });
    
    router.post('/join', (req, res) => {
      // Logic for joining a lobby goes here
      if (req.user) {
        // Join a lobby
      } else {
        res.send("Not Logged In");
      }
    });
    =======
    import { Lobby } from './lobby';
    import { InMemoryLobbyStorage } from '../connectors/lobby/InMemoryLobbyStorage';
    
    const lobbyStorage = new InMemoryLobbyStorage();
    
    router.post('/create', async (req, res) => {
      if (req.user) {
        const lobby = new Lobby(req.user.id);
        await lobbyStorage.setLobby(lobby);
        res.json(lobby);
      } else {
        res.status(401).send("Not Logged In");
      }
    });
    
    router.post('/join', async (req, res) => {
      if (req.user && req.body.lobbyId) {
        const lobby = await lobbyStorage.getLobby(req.body.lobbyId);
        if (lobby) {
          lobby.addUser(req.user.id);
          await lobbyStorage.setLobby(lobby);
          res.json(lobby);
        } else {
          res.status(404).send("Lobby not found");
        }
      } else {
        res.status(401).send("Not Logged In");
      }
    });
    >>>>>>> updated
    ```
    
    This code will create a new lobby when a POST request is made to `/create` and the user is authenticated. The lobby is then saved to the in-memory storage and returned to the user.
    
    When a POST request is made to `/join` with a lobby ID and the user is authenticated, the user is added to the lobby and the updated lobby is saved to the in-memory storage and returned to the user. If the lobby does not exist, a 404 error is returned. If the user is not authenticated, a 401 error is returned.

--- lobby/lobbyRouter.ts
+++ lobby/lobbyRouter.ts
@@ -2,23 +2,33 @@ import express from 'express';
 
 const router = express.Router();
 
-router.post('/create', (req, res) => {
-  // Logic for creating a lobby goes here
+import { Lobby } from './lobby';
+import { InMemoryLobbyStorage } from '../connectors/lobby/InMemoryLobbyStorage';
+
+const lobbyStorage = new InMemoryLobbyStorage();
+
+router.post('/create', async (req, res) => {
   if (req.user) {
-    // Create a lobby
-    // Save the lobby to the database
-    // Return the lobby to the user
+    const lobby = new Lobby(req.user.id);
+    await lobbyStorage.setLobby(lobby);
+    res.json(lobby);
   } else {
-    res.send("Not Logged In");
+    res.status(401).send("Not Logged In");
   }
 });
 
-router.post('/join', (req, res) => {
-  // Logic for joining a lobby goes here
-  if (req.user) {
-    // Join a lobby
+router.post('/join', async (req, res) => {
+  if (req.user && req.body.lobbyId) {
+    const lobby = await lobbyStorage.getLobby(req.body.lobbyId);
+    if (lobby) {
+      lobby.addUser(req.user.id);
+      await lobbyStorage.setLobby(lobby);
+      res.json(lobby);
+    } else {
+      res.status(404).send("Lobby not found");
+    }
   } else {
-    res.send("Not Logged In");
+    res.status(401).send("Not Logged In");
   }
 });
 
